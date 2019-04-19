208348badde98b68e4cfc3d596232a09f4b00368
Ricardo <ricardo85x@gmail.com>
2019-04-18 21:34:01 -0300

    aula 18 done

--- aula2/blockchain.js
+++ aula2/blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
     constructor() {
@@ -11,6 +12,39 @@ class Blockchain {
             data
         })
         this.chain.push(newBlock)
+    }
+
+    static isValidChain( le_chain ){
+
+
+        // check valid genesis
+        if(  JSON.stringify(le_chain[0]) !== JSON.stringify(Block.genesis()) ){
+            return false;
+        } 
+
+        for (let i = 1 ; i< le_chain.length; i++ ) {
+
+            // vai criar uma variavel para cada atributo  do bloco
+            const { timestamp, lastHash, hash, data }  = le_chain[i]
+            const lastHashAtual = le_chain[i - 1].hash;
+
+            if (lastHashAtual !== lastHash ){
+                return false;
+            }
+
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if (validatedHash !=  hash){
+                return false;
+            }
+
+
+        }
+
+        return  true;
+
+
+
     }
 }
 
