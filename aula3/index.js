c85bc26cd0ef2a943ddf7dc6d2c5b05cf6e3d9e6
Ricardo <ricardo85x@gmail.com>
2019-05-03 15:08:41 -0300

    aula 41 DONE

--- aula3/index.js
+++ aula3/index.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain =  require('./blockchain');
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain })
 
+setTimeout(() =>  pubsub.broadcastChain(), 1000);
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -17,9 +20,18 @@ app.post('/api/mine', (req, res) => {
 
     blockchain.addBlock( { data });
 
+    pubsub.broadcastChain();
     res.redirect('/api/blocks');
 });
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true' ){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`)
